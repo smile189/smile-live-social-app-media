d7cf61e58c892454a7b6f31b50c6856d4cb24e87
Bm8725 <211398250+Bm8725@users.noreply.github.com>
2026-02-05 21:19:58 +0200

    landing  commit

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -9,6 +9,7 @@ import TopNav from "@/components/TopNav"; //
 export default function AppPage() {
   const [items] = useState(Array.from({ length: 5 }, (_, i) => ({ id: i })));
 
+  
   return (
     <div className="h-screen w-full bg-black overflow-hidden relative font-sans">
       

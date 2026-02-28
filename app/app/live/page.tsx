76c07afe68caa9c58be4e957dedb9b1a3ac60203
smile189 <email-ul-tau-de-github@exemplu.com>
2026-02-28 12:38:31 +0200

    Smile Live: chat private

--- app/app/live/page.tsx
+++ app/app/live/page.tsx
@@ -53,9 +53,7 @@ export default function MainLive() {
     id="chat-mount-point" 
     className="h-[350px] w-full max-w-lg mb-4 px-4 md:mx-auto flex flex-col items-center"
   >
-     <div className="w-full h-full flex justify-center">
-        <ChatLive />
-     </div>
+<ChatLive streamerId={params.username} />
   </div>
 
 </div>

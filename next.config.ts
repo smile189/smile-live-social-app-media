af91953bd920b253ae6c46acd80f643ff2c27741
smile189 <email-ul-tau-de-github@exemplu.com>
2026-03-02 14:48:49 +0200

    Smile Live: region change Germany

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 1. Am eliminat experimental.appDir deoarece este activat implicit în v16
+  experimental: {
+    appDir: true,
+  },
   images: {
-    // 2. Am înlocuit 'domains' (deprecated) cu 'remotePatterns' (standard modern)
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'commondatastorage.googleapis.com',
-      },
+    domains: [
+      "images.pexels.com",
+      "images.unsplash.com",
+      "commondatastorage.googleapis.com",
     ],
   },
 };

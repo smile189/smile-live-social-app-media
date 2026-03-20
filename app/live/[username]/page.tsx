7de3338f758d6d0f4b3dc3ba040dd8206fc39c93
smile189 <email-ul-tau-de-github@exemplu.com>
2026-03-20 22:57:25 +0200

    Smile Live: live smile

--- app/live/[username]/page.tsx
+++ app/live/[username]/page.tsx
@@ -11,7 +11,7 @@ import { useParams } from 'next/navigation';
 import { createBrowserClient } from "@supabase/ssr";
 import ChatLive from '@/components/ChatLive';
 import FLive from '@/components/FLive';
-import TS4XSynth from '@/components/ts4xsynth';
+
 
 export default function MainLive() {
   const params = useParams();
@@ -64,12 +64,7 @@ export default function MainLive() {
       </div>
 
 
-       
-       {/* 2. TS4X SYNTH - Acum cu interfața corectă pentru build */}
-    <TS4XSynth 
-      isOpen={!!streamerId} 
-      onClose={() => setStreamerId(null)} 
-    />
+
 
       {/* 3. COMPONENTA DE CHAT (Z-INDEX MAI MIC CA MAGAZINUL) */}
       <div className="relative z-[50] mt-auto w-full flex flex-col items-center pointer-events-none">
--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,7 @@ const nextConfig = {
       },
     ],
   },
+  
 };
 
 export default nextConfig;

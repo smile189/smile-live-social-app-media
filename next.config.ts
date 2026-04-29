82b381734ad3ce220f3a9452d3381dd6f717b1d4
smile189 <email-ul-tau-de-github@exemplu.com>
2026-04-29 22:56:08 +0300

    rebrand logo

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,22 @@
-import withPWA from "@ducanh2912/next-pwa";
-
-const withPWAConfig = withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.pexels.com" },
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "commondatastorage.googleapis.com" },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "commondatastorage.googleapis.com",
+      },
     ],
   },
+  
 };
 
-export default withPWAConfig(nextConfig);
+export default nextConfig;
